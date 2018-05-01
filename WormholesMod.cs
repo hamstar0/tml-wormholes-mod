8d428ecb258728507e1c0b2f1ef01f42cfc86772
hamstar0 <webutek@gmail.com>
2018-05-01 02:11:53 -0700

    v1.7.2.1 * Fixed API use of Mod.Call() * Refactored net code to use PacketProtocol

--- WormholesMod.cs
+++ WormholesMod.cs
@@ -119,6 +119,21 @@ namespace Wormholes {
 
 		////////////////
 
+		public override object Call( params object[] args ) {
+			if( args.Length == 0 ) { throw new Exception( "Undefined call type." ); }
+
+			string call_type = args[0] as string;
+			if( args == null ) { throw new Exception( "Invalid call type." ); }
+
+			var new_args = new object[args.Length - 1];
+			Array.Copy( args, 1, new_args, 0, args.Length - 1 );
+
+			return WormholesAPI.Call( call_type, new_args );
+		}
+
+
+		////////////////
+
 		public override void PostDrawInterface( SpriteBatch sb ) {
 			// Clients and single only (redundant?)
 			if( Main.netMode == 2 ) { return; }
